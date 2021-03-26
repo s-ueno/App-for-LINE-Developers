15a665ba0246cdbd8683e1f8e161f5538e592e62
Shinichi Ueno <uen.agile@gmail.com>
2021-03-26 16:36:33 +0900

    いろいろ勉強になった(^^)/

--- src/views/richmenu/_hooks/useFieldByUri.ts
+++ src/views/richmenu/_hooks/useFieldByUri.ts
@@ -8,14 +8,15 @@ export function useFieldByUri(
     action: uriAction,
     validate: { validator?: () => boolean }) {
     const { t } = useTranslation();
-    const required = (newValue: string) => {
+    const required = (newValue: string, setValue: (newValue) => void) => {
+        setValue(newValue);
         if (!newValue?.trim()) {
             return t("richmenu.validate.required");
         }
         return null;
     }
-    const label = useValidatedState(required, action.label);
-    const uri = useValidatedState(required, action.uri);
+    const label = useValidatedState(x => required(x, y => action.label = y), action.label);
+    const uri = useValidatedState(x => required(x, y => action.uri = y), action.uri);
     const arrayValidator = useValidatedStateArray(label, uri);
     validate.validator = () => {
         return !arrayValidator.validate().hasError;
