4df4c4ad5c5325eb2fa63889c0194b0e30be3542
Shinichi Ueno <uen.agile@gmail.com>
2021-03-28 21:31:18 +0900

    ダメ過ぎたｗ

--- src/AppStore.ts
+++ src/AppStore.ts
@@ -10,7 +10,7 @@ const persistConfig = {
     key: 'root',
     storage,
     /** localstorage に状態保存をしてもよいというホワイトリスト */
-    whitelist: ["customTheme", "account", "channel"]
+    whitelist: ["customTheme", "account"]
 }
 const persistedReducer = persistReducer(persistConfig, rootReducers)
 const logger = createLogger({
