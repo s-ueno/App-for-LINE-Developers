e625afaeb8ad2beee312e0f94b234d826398e59e
Shinichi Ueno <uen.agile@gmail.com>
2021-03-28 10:05:48 +0900

    a

--- src/views/richmenu/_hooks/useSendRichmenu.tsx
+++ src/views/richmenu/_hooks/useSendRichmenu.tsx
@@ -22,21 +22,21 @@ export function useSendRichmenu() {
             return;
         }
 
-        const buff = await fetch(imageSrc);
-        const arrayBuffer = await buff.arrayBuffer();
-        const buffer = Buffer.from(arrayBuffer);
+        // const buff = await fetch(imageSrc);
+        // const arrayBuffer = await buff.arrayBuffer();
+        // const buffer = Buffer.from(arrayBuffer);
 
+        // const buff2 = await fetch(imageSrc);
+        // const blob = await buff2.blob();
+        // const file = new File([blob], "menu.png", { type: "image/png" });
+        // const newBlob = await resize(file, richmenu.size);
 
-        const buff2 = await fetch(imageSrc);
-        const blob = await buff2.blob();
-        const file = new File([blob], "menu.png", { type: "image/png" });
-        const newBlob = await resize(file, richmenu.size);
-
+        const blob = await resizeAsync(imageSrc, richmenu.size);
         const result = await webServiceAsync<any, { richmenuId: string }>(
             "api/updateRichmenu", {
             token: channel.token,
             richmenu,
-            buffer: newBlob,
+            buffer: blob,
         });
 
         if (result) {
@@ -56,10 +56,44 @@ export function useSendRichmenu() {
     };
     return updateRichmenuAsync;
 }
+async function resizeAsync(src: string, size: { width: number, height: number }) {
+    const imageLoadAsync = (src) => {
+        return new Promise<HTMLImageElement>((resolve, reject) => {
+            const image = new Image();
+            image.src = src;
+            image.onload = e => {
+                resolve(image);
+            }
+        });
+    }
 
+    const resize = (image: HTMLImageElement, size: { width: number, height: number }) => {
+        return new Promise<Blob>((resolve, reject) => {
+            const canvas = document.createElement("canvas");
+            canvas.width = size.width;
+            canvas.height = size.height;
+            const ctx = canvas.getContext('2d');
+            if (ctx) {
+                ctx.drawImage(canvas, 0, 0, canvas.width, canvas.height, 0, 0, size.width, size.height);
+                canvas.toBlob(x => {
+                    if (x) {
+                        resolve(x);
+                    } else {
+                        reject();
+                    }
+                });
+            } else {
+                reject();
+            }
+        });
+    }
+    const image = await imageLoadAsync(src);
+    const resized = await resize(image, size);
+    return resized;
+}
 async function resize(file: Blob, size: { width: number, height: number }) {
     return new Promise<Blob>(resolve => {
-        Resizer.imageFileResizer(file, size.width, size.height, 'PNG', 100, 0,
+        Resizer.imageFileResizer(file, size.width, size.height, 'png', 100, 0,
             blob => {
                 resolve(blob as Blob);
             },
@@ -67,3 +101,13 @@ async function resize(file: Blob, size: { width: number, height: number }) {
         );
     });
 }
+// function resizedCanvas(blob, size: { width: number, height: number }) {
+//     const tmpCanvas = document.createElement('canvas');
+//     tmpCanvas.width = size.width;
+//     tmpCanvas.height = size.height;
+//     const ctx = tmpCanvas.getContext('2d');
+//     if (ctx) {
+//         ctx.drawImage(canvas, 0, 0, canvas.width, canvas.height, 0, 0, size.width, size.height);
+//     }
+//     return tmpCanvas;
+// }
