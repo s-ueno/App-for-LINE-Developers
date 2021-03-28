019e5a4b04f443a0bb938ed0e50f63ee3f2b781c
Shinichi Ueno <uen.agile@gmail.com>
2021-03-28 17:06:06 +0900

    F

--- src/api/updateRichmenu/index.ts
+++ src/api/updateRichmenu/index.ts
@@ -68,7 +68,7 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
         const newRichMenuId = await client.createRichMenu(richmenu);
 
         const buffer = Buffer.from(request.buffer.data);
-        const stream = fs.createReadStream(buffer);
+        const readable = bufferToStream(buffer);
         // await client.setRichMenuImage(newRichMenuId, request.buffer.data, "image/png");
         // const readable = bufferToStream(request.buffer.data);
         // await client.setRichMenuImage(newRichMenuId, request.buffer.data);
