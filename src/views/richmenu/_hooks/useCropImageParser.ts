3859b0c8c8448afc2311765496720114c26a48e3
Shinichi Ueno <uen.agile@gmail.com>
2021-03-29 15:17:17 +0900

    ぎやーーーーー

--- src/views/richmenu/_hooks/useCropImageParser.ts
+++ src/views/richmenu/_hooks/useCropImageParser.ts
@@ -14,6 +14,7 @@ export function useCropImageParser(unit: "px" | "%" = "px") {
             let scaleX = 1;
             let scaleY = 1;
             if (bounds.serverScale) {
+                bounds.serverScale = false;
                 scaleX = richMenuObject.size.width / image.width;
                 scaleY = richMenuObject.size.height / image.height;
             } else {
